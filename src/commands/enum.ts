7c175dd5d32c7362635c23fd9009c3cb17431616
N0ts <mail@n0ts.top>
2026-04-22 20:35:08 +0800

    chore: 压缩枚举格式为单行 key=val, val=val

--- src/commands/enum.ts
+++ src/commands/enum.ts
@@ -1,5 +1,4 @@
 import { Command } from 'commander';
-import { info } from '../utils/printer';
 import { ENUM_DOC } from '../constants/enums';
 
 export function registerEnumCommand(program: Command) {
@@ -8,6 +7,5 @@ export function registerEnumCommand(program: Command) {
     .description('查看所有枚举值说明')
     .action(async () => {
       console.log(ENUM_DOC);
-      info('\n品牌和分类请使用实际 ID，可通过供应商详情中的 brands/categories 字段查看已有绑定');
     });
 }
