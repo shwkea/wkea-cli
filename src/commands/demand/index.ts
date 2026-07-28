6cd227ab53d898f9270d92ea17d054c52fa962a6
N0ts <mail@n0ts.top>
2026-07-28 16:36:43 +0800

    还原 demand parse 命令，去掉硬编码 token 默认值

--- src/commands/demand/index.ts
+++ src/commands/demand/index.ts
@@ -1,10 +1,12 @@
 import { Command } from 'commander';
 import { registerCrudCommands } from './crud';
 import { registerListCommand } from './list';
+import { registerParseCommand } from './parse';
 import { registerProcessCommand } from './process';
 
 export function registerDemandCommands(program: Command) {
   registerCrudCommands(program);
   registerListCommand(program);
   registerProcessCommand(program);
+  registerParseCommand(program);
 }
